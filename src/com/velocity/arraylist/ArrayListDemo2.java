32a9d6436a91cfe6f4ca8fbd24076c043135702f
pratapwaghmare <pratapwaghmare99@gmail.com>
2023-01-08 15:04:09 +0530

    Added Arraylist 2 file

---
+++ src/com/velocity/arraylist/ArrayListDemo2.java
@@ -0,0 +1,20 @@
+package com.velocity.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo2 {
+	public static void main(String[] args) {
+		
+	
+	List <String> list = new ArrayList<String>();
+	
+	list.add("Ajay");
+	list.add("Rohan");
+	list.add("Dipak");
+	list.add("Rahul");
+	
+	System.out.println(list);
+
+}
+}
